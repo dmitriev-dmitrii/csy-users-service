c039dd2d70113076416af01b5523064a490ca138
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-05-14 20:19:04 +0300

    set global prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.setGlobalPrefix('api/users/');
+  await app.listen(4000);
+  console.log('app listen: http://localhost:4000/api/users/');
 }
 bootstrap();
